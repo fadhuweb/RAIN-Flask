9518d8b213d27d6b13e6fef1c143aeba31e1e18c
fadhuweb <f.abdulazee@alustudent.com>
2024-05-27 14:44:49 +0100

    Initial commit

---
+++ Form/Form/app1.py
@@ -0,0 +1,35 @@
+#Import libraries
+from flask import Flask, request, render_template
+import csv
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    #get details from form
+    name = request.form.get('name')
+    age = request.form.get("age")
+    gender = request.form.get("gender")
+    email = request.form.get ("email")
+
+    #define the csv filepath
+    csv_file ='submissions'
+    # Check if file path exists
+    if not os.path.exists(csv_file):
+        with open(csv_file, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Name', 'Age', 'Gender', 'Email'])
+    writer.writer(['name', 'age', 'gender', 'email'])
+
+#return the results
+return render_template('result.html', name= name, age=age, gender=gender, email=email)
+
+          
+if __name__ == '__main__':
+    app.run(debug=True)
